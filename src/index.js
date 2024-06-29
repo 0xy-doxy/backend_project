0b321537a25f7c478562e4bee298e07566af756f
0xy-doxy <hopelesshackerman007@gmail.com>
2024-06-29 13:10:43 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -8,3 +8,11 @@ dotenv.config({
 })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`server is running at port : ${process.env.PORT} `);
+    })
+})
+.catch((err) => {
+    console.log("MONGO DB connection error !!!", err);
+})
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+const aysncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+export {aysncHandler}
+
+// another way to write above function :
+
+// const aysncHandler = (fn) => async(req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
