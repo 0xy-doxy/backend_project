fc4376fbe193b0e25d817887afcaf36328766a60
0xy-doxy <hopelesshackerman007@gmail.com>
2024-07-22 23:20:26 +0530

    bugs fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -257,7 +257,7 @@ const changeCurrentPassword = asyncHandler(async(req, res) => {
 const getCurrentUser = asyncHandler(async(req, res) => {
     return res
     .status(200)
-    .json(200, req.user, "current user fetched successfully")
+    .json( new ApiResponse (200, req.user, "current user fetched successfully"))
 })
 
 const updateAccountDetails = asyncHandler(async(req, res) => {
@@ -267,7 +267,7 @@ const updateAccountDetails = asyncHandler(async(req, res) => {
         throw new ApiError(400, "all fields are required !!")
     }
 
-    const user = User.findByIdAndUpdate(
+    const user = await User.findByIdAndUpdate(
         req.user?._id,
         {
             $set:{
